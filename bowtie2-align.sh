48b0588665985a69e1c5d53cb8a00ad4e27d6f98
Stas Malavin <stas.malavin@gmail.com>
2020-11-17 00:43:23 +0300

    Some fixes

--- bowtie2-align.sh
+++ bowtie2-align.sh
@@ -11,21 +11,27 @@ align.sh -h
 -r\tReference genome (fasta)
 -t\tNumber of threads
 -h\tThis help
-This script needs bowtie2, awk, and samtools in your \$PATH\n"; exit;;
-		a) READ1=$OPTARG;;
-		b) READ2=$OPTARG;;
-		r) REF=$OPTARG;;
-		t) THREADS=$OPTARG;;
-		\?) echo "Invalid option: -$OPTARG" >&2;;
-		:) echo "Option -$OPTARG requires an argument" >&2;;
+This script needs bowtie2, awk, and samtools in the \$PATH\n"; exit;;
+		a) READ1="$OPTARG";;
+		b) READ2="$OPTARG";;
+		r) REF="$OPTARG";;
+		t) THREADS="$OPTARG";;
+		\?) echo "Invalid option: -$OPTARG"; exit >&2;;
+		:) echo "Option -$OPTARG requires an argument"; exit >&2;;
 	esac
 done
+
+# For debugging purpose
+#rm log
+#echo "read1: $READ1" >> log
+#echo "read2: $READ2" >> log
+#echo "ref: $REF" >> log
 
 # Build the index
 bowtie2-build $REF $REF
 
 # Align the reads with bowtie2
-bowtie2 -x $REF --threads $THREADS --very-sensitive -1 $READ1 -2 $READ2 -S |\
+bowtie2 -1 $READ1 -2 $READ2 -x $REF --threads $THREADS --very-sensitive -S - |\
 	awk 'BEGIN{FS = "[\t]"}{if (($3 != "*" && (length($10)>=20)) || $1 ~ /^@/ ){print}}' |\
         samtools view -Sb - |\
 	samtools sort -o $REF-bowtie_mapped.bam
