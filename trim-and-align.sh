3dae1cd733909cf4421e0c958b911e031a5094cb
Stas Malavin <stas.malavin@gmail.com>
2020-11-16 22:15:45 +0300

    Remove Trimmomatic code from the main script

--- trim-and-align.sh
+++ trim-and-align.sh
@@ -1,33 +1,25 @@
 #!/bin/bash
 
 # Parse arguments
-while getopts :ha:b:d:r:t: opt; do
+while getopts :ha:b:r:t: opt; do
 	case $opt in 
 		h) printf "USAGE:
-trim-and-align.sh -a <forward reads> -b <reverse reads> -d <adapter file> -r <reference genome> -t <number of threads>
-trim-and-align.sh -h
+align.sh -a <forward reads> -b <reverse reads> -r <reference genome> -t <number of threads>
+align.sh -h
 -a\tforward reads
 -b\treverse reads
--d\tAdapter file (fasta)
 -r\tReference genome (fasta)
 -t\tNumber of threads
 -h\tThis help
-This script needs TrimmomaticPE, bowtie2, awk, and samtools in your \$PATH\n"; exit;;
+This script needs bowtie2, awk, and samtools in your \$PATH\n"; exit;;
 		a) READ1=$OPTARG;;
 		b) READ2=$OPTARG;;
-		d) ADAPTER=$OPTARG;;
 		r) REF=$OPTARG;;
 		t) THREADS=$OPTARG;;
 		\?) echo "Invalid option: -$OPTARG" >&2;;
 		:) echo "Option -$OPTARG requires an argument" >&2;;
 	esac
 done
-
-# Trim the reads
-TrimmomaticPE -threads $THREADS -phred33 \
-	$READ1 $READ2 $READ1-pair.fq.gz $READ1-unpair.fq.gz \
-	$READ2-pair.fq.gz $READ2-unpair.fq.gz \
-	ILLUMINACLIP:$ADAPTER:2:40:15 HEADCROP:10 MINLEN:230
 
 # Build the index
 bowtie2-build $REF $REF
