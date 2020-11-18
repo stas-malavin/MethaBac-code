9d2c3f55f0fcd9d55ed22d3451a5a12474b4e78c
Stas Malavin <stas.malavin@gmail.com>
2020-11-18 23:40:19 +0300

    Adapt the trim.sh script to the newest (0.39) version of Trimmomatic

--- trim.sh
+++ trim.sh
@@ -3,19 +3,14 @@
 
 # Pass arguments to this script as positional
 BASE=$1
-ADAPTER=$2
-THREADS=16
+ADAPTER="/home/stas/Programs/Trimmomatic-0.39/adapters/NexteraPE-PE.fa"
+THREADS=8
 
 # Trim the reads
-#TrimmomaticPE -threads $THREADS -phred33 -basein $BASE -baseout $BASE-trimmed.fq.gz \
-	#ILLUMINACLIP:$ADAPTER:2:40:15:1:true \
-	#HEADCROP:10 \
-	#SLIDINGWINDOW:4:15 \
-	#MINLEN:230 \
-#$Input Read Pairs: 3320440 Both Surviving: 2274250 (68.49%) Forward Only Surviving: 93298 (2.81%) Reverse Only Surviving: 422721 (12.73%) Dropped: 530171 (15.97%)
-
-TrimmomaticPE -threads $THREADS -phred33 -basein $BASE -baseout $BASE-trimmed.fq.gz \
+java -jar /home/stas/Programs/Trimmomatic-0.39/trimmomatic-0.39.jar PE \
+	-threads $THREADS -phred33 -basein $BASE -baseout $BASE-trimmed.fq.gz \
 	ILLUMINACLIP:$ADAPTER:2:40:15:1:true \
 	HEADCROP:10 \
+	SLIDINGWINDOW:4:15 \
 	MINLEN:230
-#$Input Read Pairs: 3320440 Both Surviving: 2413096 (72.67%) Forward Only Surviving: 0 (0.00%) Reverse Only Surviving: 907339 (27.33%) Dropped: 5 (0.00%)
+#Input Read Pairs: 3320440 Both Surviving: 2270707 (68.39%) Forward Only Surviving: 174184 (5.25%) Reverse Only Surviving: 23857 (0.72%) Dropped: 851692 (25.65%)
